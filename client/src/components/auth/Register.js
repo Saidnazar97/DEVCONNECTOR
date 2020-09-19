8de8c83d222c25057b1a1ad1786bf7e9420ab0c3
Saidnazar97 <lifedada98@gmail.com>
2020-09-19 01:36:16 -0700

    Dashboard setup

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,54 +1,72 @@
-import PropTypes from 'prop-types';
-import React, { Fragment, useState } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import PropTypes from "prop-types";
 
-import { setAlert } from '../../actions/alert';
-import { register } from '../../actions/auth';
-
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
-    name: ``,
-    email: ``,
-    paaword: ``,
-    password2: ``
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
   });
 
   const { name, email, password, password2 } = formData;
 
-  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = async e => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    if(password !== password2) {
-      setAlert(`Password do not match`, `danger`);
+    if (password !== password2) {
+      setAlert("Passwords do not match", "danger");
     } else {
-     register({ name, email, password }); 
+      register({ name, email, password });
     }
   };
 
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
   return (
     <Fragment>
-     <h1 className="large text-primary">Sign Up</h1>
-      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-      <form className="form" onSubmit={e => onSubmit(e)}>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Create Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} />
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onChange}
+          />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
-          <small className="form-text"
-            >This site uses Gravatar so if you want a profile image, use a
-            Gravatar email</small
-          >
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
         </div>
         <div className="form-group">
           <input
             type="password"
             placeholder="Password"
             name="password"
-          
-            value={password} onChange={e => onChange(e)}
+            value={password}
+            onChange={onChange}
           />
         </div>
         <div className="form-group">
@@ -56,8 +74,8 @@ const Register = ({ setAlert, register }) => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            
-            value={password2} onChange={e => onChange(e)}
+            value={password2}
+            onChange={onChange}
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -71,7 +89,12 @@ const Register = ({ setAlert, register }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
-}
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
 
-export default connect(null, {setAlert, register}) (Register);
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
